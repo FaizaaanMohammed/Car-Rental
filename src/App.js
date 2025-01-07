be94c841324342aa29bcabd7b7113e8ccc3bffb3
unknown <faizanmohammed746@gmail.com>
2025-01-07 22:04:03 +0530

    add new section in this prject

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './cms/Home/Home';
 import Footer from './Layout/Footer/Footer';
 
+
 function App() {
   return (
     <>
--- src/cms/Home/Home.jsx
+++ src/cms/Home/Home.jsx
@@ -1,4 +1,14 @@
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import Navbar from "../../Layout/Navbar/Navbar";
 import BoltIcon from "@mui/icons-material/Bolt";
@@ -7,12 +17,74 @@ import ThermostatAutoIcon from "@mui/icons-material/ThermostatAuto";
 import Hero from "../Home/Home.module.css";
 import SpeedIcon from "@mui/icons-material/Speed";
 import gearbox from "../../Assets/images/AboutPicture.png";
+import brand1 from "../../Assets/images/brand1.png";
+import brand2 from "../../Assets/images/brand 2.png";
+import brand3 from "../../Assets/images/brand3.png";
+import EvStationIcon from "@mui/icons-material/EvStation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Pagination } from "swiper/modules";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
 
 const Home = () => {
   const temp = [
     { icon: <ThermostatAutoIcon />, title: "34°", descr: "TEMPERATURE" },
     { icon: <SpeedIcon />, title: "873", descr: "MILEAGE" },
     { icon: <BoltIcon />, title: "94%", descr: "BATTERY" },
+  ];
+
+  const cards = [
+    {
+      id: 1,
+      title: "Porsche",
+      description: "Description of Car 1",
+      img: brand1,
+      model: "Turbo S",
+      price: "$175,900",
+    },
+    {
+      id: 2,
+      title: "Porsche",
+      description: "Description of Car 2",
+      img: brand2,
+      model: "Taycan",
+      price: "$114,900",
+    },
+    {
+      id: 3,
+      title: "Porsche",
+      description: "Description of Car 3",
+      img: brand3,
+      model: "Turbo S Cross",
+      price: "$175,900",
+    },
+    {
+      id: 4,
+      title: "Porsche",
+      description: "Description of Car 1",
+      img: brand1,
+      model: "Turbo S",
+      price: "$175,900",
+    },
+    {
+      id: 5,
+      title: "Porsche",
+      description: "Description of Car 2",
+      img: brand2,
+      model: "Taycan",
+      price: "$114,900",
+    },
+    {
+      id: 6,
+      title: "Porsche",
+      description: "Description of Car 3",
+      img: brand3,
+      model: "Turbo S Cross",
+      price: "$175,900",
+    },
   ];
   return (
     <>
@@ -179,19 +251,38 @@ const Home = () => {
       </Box>
       {/* // end of hero section */}
       {/* start technology part */}
-      <Box sx={{ backgroundColor: "#0A0A0B", padding: {xs:"30px 0px",sm:"100px 0px"} }}>
+      <Box
+        sx={{
+          backgroundColor: "#0A0A0B",
+          padding: { xs: "30px 0px", sm: "100px 0px" },
+        }}
+      >
         <Container maxWidth="lg">
           <Grid container>
-            <Grid xs={12} sm={12} md={6} sx={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-              <Box sx={{ position: "relative",paddingBottom:{sm:"80px",md:"0px",xs:"80px"} }} >
+            <Grid
+              xs={12}
+              sm={12}
+              md={6}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "relative",
+                  paddingBottom: { sm: "80px", md: "0px", xs: "80px" },
+                }}
+              >
                 <img src={gearbox} alt="" className={Hero.technologyPicture} />
                 <Box
                   sx={{
                     width: "197px",
                     height: "104px",
                     position: "absolute",
-                    right: {xs:"-1px",md:"-5px"},
-                    bottom: {xs:"25px",md:"-50px"},
+                    right: { xs: "-1px", md: "-5px" },
+                    bottom: { xs: "25px", md: "-50px" },
                     borderRadius: "15px",
                     backgroundColor: "rgb(255,255,255,10%)",
                     backdropFilter: "blur(16px)",
@@ -220,7 +311,7 @@ const Home = () => {
                       fontSize: "13px",
                       textAlign: "center",
                       fontFamily: "Exo",
-                      lineHeight:"140%"
+                      lineHeight: "140%",
                     }}
                   >
                     Supercharges placed <br /> along popular routes
@@ -228,16 +319,35 @@ const Home = () => {
                 </Box>
               </Box>
             </Grid>
-            <Grid xs={12} sm={12} md={6} sx={{sm:{display:"flex",justifyContent:"center",alignItems:"center"}}}>
-              <Box sx={{display:"flex",justifyContent:"center",alignItems:"flex-start",flexDirection:"column",paddingLeft:{md:"50px"}}}>
+            <Grid
+              xs={12}
+              sm={12}
+              md={6}
+              sx={{
+                sm: {
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                },
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "flex-start",
+                  flexDirection: "column",
+                  paddingLeft: { md: "50px" },
+                }}
+              >
                 <Typography
                   sx={{
                     color: "#fff",
-                    fontSize: {xs:"20px",sm:"24px"},
+                    fontSize: { xs: "20px", sm: "24px" },
                     fontWeight: "600",
                     fontFamily: "Exo",
                     lineHeight: "140%",
-                    paddingBottom: {xs:"30px",sm:"40px"},
+                    paddingBottom: { xs: "30px", sm: "40px" },
                     paddingTop: "25px",
                   }}
                 >
@@ -246,18 +356,19 @@ const Home = () => {
                 <Typography
                   sx={{
                     color: "#BABEC4",
-                    fontSize: {xs:"15px",sm:"16px"},
+                    fontSize: { xs: "15px", sm: "16px" },
                     fontWeight: "500",
                     fontFamily: "Exo",
                     lineHeight: "140%",
-                    paddingBottom: {xs:"40px",sm:"40px"},
+                    paddingBottom: { xs: "40px", sm: "40px" },
                     letterSpacing: "0%",
                   }}
                 >
-                  See the future with high-performance electric cars <br className={Hero.break} />{" "}
-                  produced by renowned brands. They feature futuristic <br className={Hero.break} />{" "}
-                  builds and designs with new and innovative platforms <br className={Hero.break} />{" "}
-                  that last a long time.
+                  See the future with high-performance electric cars{" "}
+                  <br className={Hero.break} /> produced by renowned brands.
+                  They feature futuristic <br className={Hero.break} /> builds
+                  and designs with new and innovative platforms{" "}
+                  <br className={Hero.break} /> that last a long time.
                 </Typography>
                 <Box>
                   <Button
@@ -273,6 +384,236 @@ const Home = () => {
                 </Box>
               </Box>
             </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      {/* end technology part  */}
+      {/* start porsche brand */}
+      <Box
+        sx={{
+          backgroundColor: "#0A0A0B",
+          padding: { xs: "50px 0px", sm: "100px 0px" },
+        }}
+      >
+        <Container maxWidth="lg">
+          <Typography
+            sx={{
+              color: "#F1F2F3",
+              textAlign: "center",
+              fontSize: "24px",
+              fontWeight: "600",
+              fontFamily: "Exo",
+              lineHeight: "140%",
+              paddingBottom: {xs:"40px",sm:"80px"},
+            }}
+          >
+            Choose Your Electric Car <br /> Of The Porsche Brand
+          </Typography>
+          <Grid container>
+            <Box sx={{ width: "100%" }}>
+              <Swiper
+                modules={[Pagination]}
+                spaceBetween={20} // Space between slides
+                slidesPerView={3} // 3 slides visible at once (on X-axis)
+                pagination={{ clickable: true, type: "bullets" }} // Enable clickable dot navigation
+                direction="horizontal"
+                scrollbar={{ draggable: true }}
+                style={{ width: "100%" }}
+                breakpoints={{
+                  360: {
+                    slidesPerView: 1,
+                    spaceBetween: 10, // space between for mobile
+                  },
+                  600: {
+                    slidesPerView: 2,
+                    spaceBetween: 20, // space between for medium screens
+                  },
+                  900: {
+                    slidesPerView: 3,
+                    spaceBetween: 30, // space between for large screens
+                  },
+                }}
+                sx={{
+                  "& .swiper-pagination": {
+                    Top: "-20px!important", // Space between the swiper and the bullets
+                  },
+                  "& .swiper-pagination-bullet": {
+                    backgroundColor: "white !important", // Ensure bullets are white
+                    marginTop: "10px", // Adjust space above each bullet
+                  },
+                  "& .swiper-pagination-bullet-active": {
+                    backgroundColor: "blue !important", // Example: Change color of active bullet
+                  },
+                }}
+              >
+                {cards.map((item) => {
+                  return (
+                    <>
+                      <SwiperSlide key={item.id}>
+                        <Grid
+                          sx={{ display: "flex", justifyContent: "center" }}
+                        >
+                          <Card
+                            sx={{
+                              minWidth: 290,
+                              padding: "20px 5px",
+                              display: "flex",
+                              justifyContent: "flex-start",
+                              alignItems: "flex-start",
+                              flexDirection: "column",
+                              borderRadius: "20px",
+                              backgroundColor: "#111213",
+                              paddingBottom: "5px",
+                              overflow:"hidden"
+                            }}
+                          >
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                                sx={{
+                                  fontSize: "24px",
+                                  fontWeight: "600",
+                                  fontFamily: "Exo",
+                                  color: "#fff",
+                                }}
+                              >
+                                {item.title}
+                              </Typography>
+                              <Typography
+                                variant="body2"
+                                sx={{
+                                  color: "text.secondary",
+                                  fontSize: "16px",
+                                  fontWeight: "500",
+                                  fontFamily: "Exo",
+                                  color: "#fff",
+                                  paddingBottom: "20px",
+                                }}
+                              >
+                                {item.model}
+                              </Typography>
+                            </CardContent>
+                            <CardMedia
+                              sx={{
+                                height: 80,
+                                width: "180px",
+                                margin: "auto",
+                              }}
+                              image={item.img}
+                              title={item.title}
+                            />
+
+                            <CardContent
+                              sx={{
+                                color: "#fff",
+                                display: "flex",
+                                justifyContent: "space-between",
+                                paddingTop: "30px",
+                                paddingBottom: "8px",
+                              }}
+                            >
+                              <span
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "center",
+                                  alignItems: "center",
+                                  fontFamily: "Exo",
+                                  fontSize: "13px",
+                                  marginRight: "30px",
+                                }}
+                              >
+                                <SpeedIcon sx={{ marginRight: "8px" }} /> 3.4
+                                sec
+                              </span>
+                              <span
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "center",
+                                  alignItems: "center",
+                                  fontFamily: "Exo",
+                                  fontSize: "13px",
+                                }}
+                              >
+                                <SpeedIcon sx={{ marginRight: "8px" }} /> 3.4
+                                sec
+                              </span>
+                            </CardContent>
+                            <CardContent
+                              sx={{ color: "#fff", paddingTop: "0px" }}
+                            >
+                              <span
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "center",
+                                  alignItems: "center",
+                                  fontFamily: "Exo",
+                                  fontSize: "13px",
+                                }}
+                              >
+                                <EvStationIcon sx={{ marginRight: "8px" }} />{" "}
+                                Electric
+                              </span>
+                            </CardContent>
+                            <CardContent
+                              sx={{
+                                paddingTop: "10px",
+                                color: "#fff",
+                                paddingBottom: "5px",
+                              }}
+                            >
+                              <Typography
+                                sx={{
+                                  fontSize: "20px",
+                                  fontFamily: "Exo",
+                                  fontWeight: "600",
+                                  paddingLeft: "3px",
+                                }}
+                              >
+                                {item.price}
+                              </Typography>
+                            </CardContent>
+                            <Box
+                              sx={{
+                                width: "56px",
+                                height: "46px",
+                                backgroundColor: "#4177DC",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                borderRadius: "20px 0px 20px 0px",
+                                position: "absolute",
+                                right: "40px",
+                                bottom: "0",
+                                zIndex: "2",
+                              }}
+                              className={Hero.cardBag}
+                            >
+                              <LocalMallIcon sx={{ color: "#fff" }} />
+                            </Box>
+                            <Box
+                              sx={{
+                                width: "100px",
+                                height: "100px",
+                                position: "absolute",
+                                backgroundColor:"rgb(55,74,150,50%)",
+                                top:"10",
+                                left:"75px",
+                                filter:"blur(48px)",
+                                zIndex:"1",
+                                borderRadius:"50%",
+                                opacity:"150%"
+                              }}
+                            ></Box>
+                          </Card>
+                        </Grid>
+                      </SwiperSlide>
+                    </>
+                  );
+                })}
+              </Swiper>
+            </Box>
           </Grid>
         </Container>
       </Box>
